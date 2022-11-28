bccd53dba43f5579001b0827cf836424b36f6d7e
isymo <isymo@ADMIN>
2022-11-28 21:35:12 +0300

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <windows.h>
+
+
+int main()
+{
+	srand(time(0));
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "RU");
+
+	int arrayLength;
+
+
+	std::cout << "Введите длину массива" << std::endl;
+	std::cin >> arrayLength;
+	
+
+	int* array = new int[arrayLength];
+	std::cout << "Созданый массив" << std::endl;
+	std::cout << "-------------------------------------------" << std::endl;
+
+	for (int i = 0; i < arrayLength; i++)
+	{
+		array[i] = 0 + rand() % 9; // Случайные числа от 0 до 9
+		std::cout << array[i] << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "-------------------------------------------" << std::endl;
+	std::cout << std::endl;
+
+
+	std::cout << "Массив после удаления из его нолей" << std::endl;
+	std::cout << "-------------------------------------------" << std::endl;
+
+	int zeroCounter = 0;
+	int howMuchZeroSkipped = 0;
+
+	for (int i = 0; i < arrayLength; i++)
+	{
+		if (array[i] == 0)
+		{
+			zeroCounter++;
+		}
+	}
+
+	int* arrayWithoutZeros = new int[arrayLength - zeroCounter];
+
+	for (int i = 0; i < arrayLength; i++)
+	{
+
+		if (array[i] == 0)
+		{
+			
+			howMuchZeroSkipped++;
+			continue;
+		}
+		arrayWithoutZeros[i - howMuchZeroSkipped] = array[i];
+	}
+
+	for (int i = 0; i < (arrayLength - zeroCounter); i++)
+	{
+		std::cout << arrayWithoutZeros[i] << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "-------------------------------------------" << std::endl;
+
+}
